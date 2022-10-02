9e0eca1a14fc8056e3c75fc7de3b675ac1614715
SvetonosovS <tris23@bk.ru>
2022-10-02 23:18:51 +0300

    max2

---
+++ Program_2.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите целое первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+
+Console.Write("Введите целое второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNumber2 = int.Parse(userInput2);
+
+Console.Write("a=" + userNumber1 + ";" + "b=" + userNumber2 + " -> MAX:");
+
+if (userNumber1>userNumber2)
+{
+    Console.WriteLine(userNumber1);
+}
+else
+{
+    Console.WriteLine(userNumber2);
+}
